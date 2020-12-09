25d246bc4031aef42b1ce9ff8c4be2e73fb02eb5
Morten Falkegård <morten.falkegard@nina.no>
2020-12-09 15:17:00 +0100

    Quick edits to increase readability.

--- src/catch-distribution.R
+++ src/catch-distribution.R
@@ -2,8 +2,8 @@
 
 #library(triangle)
 library(openxlsx)
-
-setwd("C:/Users/morten.falkegard/OneDrive - NINA/Tana monitoring group/Status assessment files")
+library(tidyverse)
+library(rio)
 
 # variables defining starting year and number of years used in estimation
 StartingYear <- "2018"
@@ -11,7 +11,7 @@ NumberOfYears <- 1
 
 # obtain list of rivers with spawning targets, 1.5 kg proportions and PFA method
 river_list <- read.xlsx("catch-dist-riverlist.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
-NumberOfRivers <- nrow(river_list)-2 # -2 because last items of list are Other tribs and Tana total, currently not supported
+NumberOfRivers <- nrow(river_list) - 2 # -2 because last items of list are Other tribs and Tana total, currently not supported
 
 # obtain coastal catch distribution numbers (proportion of salmon from Tana in different coastal regions)
 coastal_dist <- read.xlsx("coastal-dist-table.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
@@ -19,7 +19,7 @@ coastal_dist <- read.xlsx("coastal-dist-table.xlsx", sheet = 1, startRow = 1, co
 # obtain annual list of salmon catch (biomass) from coastal regions and Tana main stem 
 coastal_MS_catch <- read.xlsx("coastal-MS-catch.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
 j <- which(coastal_MS_catch$Year == StartingYear)
-YearList <- coastal_MS_catch[j:(j+NumberOfYears-1), "Year"]
+YearList <- coastal_MS_catch[j:(j + NumberOfYears - 1), "Year"]
 
 # obtain annual list of main stem catch proportions of the different Tana stocks
 MS_catch_proportions <- read.xlsx("catch-dist-MS-props.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
@@ -28,72 +28,72 @@ MS_catch_proportions <- read.xlsx("catch-dist-MS-props.xlsx", sheet = 1, startRo
 GBM_assess <- read.xlsx("GBM_results.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
 
 # variable declarations
-TributaryCatch <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-TributaryFemProp <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-TributarySpawn <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-TributaryExpl <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-TributaryGBMAttain <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-MainStemCatch <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-PFA_MS <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-PFA_MS_15 <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-PFA_trib <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-PFA15_prop <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-PFA_coast <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-PFA_coast_fem <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
+TributaryCatch <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+TributaryFemProp <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+TributarySpawn <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+TributaryExpl <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+TributaryGBMAttain <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+MainStemCatch <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+PFA_MS <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+PFA_MS_15 <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+PFA_trib <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+PFA15_prop <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+PFA_coast <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+PFA_coast_fem <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
 TanaCoastalCatch <- vector()
-StockCoastalCatch <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-StockTotalCatch <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-OverExploitation <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
-MaxTotalExploitation <- matrix(nrow=NumberOfYears, ncol=NumberOfRivers)
+StockCoastalCatch <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+StockTotalCatch <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+OverExploitation <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
+MaxTotalExploitation <- matrix(nrow = NumberOfYears, ncol = NumberOfRivers)
 
 # distribute the Tana main stem catch on the different stocks
 j <- which(coastal_MS_catch$Year == StartingYear)
 for (i in 1:NumberOfRivers) {
-  MainStemCatch[1:NumberOfYears, i] <- coastal_MS_catch[j:(j+NumberOfYears-1), "TanaMS"] *
-    MS_catch_proportions[j:(j+NumberOfYears-1), i+1]
+  MainStemCatch[1:NumberOfYears, i] <- coastal_MS_catch[j:(j + NumberOfYears - 1), "TanaMS"] *
+    MS_catch_proportions[j:(j + NumberOfYears - 1), i + 1]
 }
 
 for (i in 1:NumberOfRivers) { 
   CurrentRiver <- as.character(river_list[i, "RiverFullName"])
-  if(river_list[i, "PFA_method"]==1) { # Tana MS and tribs with very limited fisheries, treat as special cases
+  if(river_list[i, "PFA_method"] == 1) { # Tana MS and tribs with very limited fisheries, treat as special cases
     j <- which(GBM_assess$RiverYear == StartingYear & GBM_assess$RiverList == CurrentRiver)
     TributaryCatch[1:NumberOfYears, i] <- 0
-    TributaryFemProp[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "FemPropForFile"]
-    TributarySpawn[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "Spawn_W_mod"]
+    TributaryFemProp[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "FemPropForFile"]
+    TributarySpawn[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "Spawn_W_mod"]
     TributaryExpl[1:NumberOfYears, i] <- 0
-    TributaryGBMAttain[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "GBM_maal"]
-  } else if(CurrentRiver=="Other tribs") { # Other tribs, special case
+    TributaryGBMAttain[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "GBM_maal"]
+  } else if(CurrentRiver == "Other tribs") { # Other tribs, special case
     TributaryCatch[1:NumberOfYears, i] <- 0
     TributaryFemProp[1:NumberOfYears, i] <- 0.5
     TributaryExpl[1:NumberOfYears, i] <- 0
-    TributarySpawn[1:NumberOfYears, i] <- (MainStemCatch[1:NumberOfYears, i]*TributaryFemProp[1:NumberOfYears,i])/0.5 # assume 0.5 main stem exploitation rate for now
-    TributaryGBMAttain[1:NumberOfYears, i] <- TributarySpawn[1:NumberOfYears, i]/river_list[i, "GBM_med"]
+    TributarySpawn[1:NumberOfYears, i] <- (MainStemCatch[1:NumberOfYears, i] * TributaryFemProp[1:NumberOfYears, i]) / 0.5 # assume 0.5 main stem exploitation rate for now
+    TributaryGBMAttain[1:NumberOfYears, i] <- TributarySpawn[1:NumberOfYears, i] / river_list[i, "GBM_med"]
   } else {
     j <- which(GBM_assess$RiverYear == StartingYear & GBM_assess$RiverList == CurrentRiver)
-    TributaryCatch[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "CatchForFile"]
-    TributaryFemProp[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "FemPropForFile"]
-    TributarySpawn[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "Spawn_W_mod"]
-    TributaryExpl[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "Expl_M"]
-    TributaryGBMAttain[1:NumberOfYears, i] <- GBM_assess[j:(j+NumberOfYears-1), "GBM_maal"]
+    TributaryCatch[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "CatchForFile"]
+    TributaryFemProp[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "FemPropForFile"]
+    TributarySpawn[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "Spawn_W_mod"]
+    TributaryExpl[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "Expl_M"]
+    TributaryGBMAttain[1:NumberOfYears, i] <- GBM_assess[j:(j + NumberOfYears - 1), "GBM_maal"]
   }
 }
 
 for (i in 1:NumberOfRivers) {
   CurrentRiver <- as.character(river_list[i, "RiverFullName"])
-  if(river_list[i, "PFA_method"]==1) { # Tana MS and tribs with Genmix assessment
-    PFA_trib[1:NumberOfYears, i] <- TributarySpawn[1:NumberOfYears, i]/TributaryFemProp[1:NumberOfYears, i]+
+  if(river_list[i, "PFA_method"] == 1) { # Tana MS and tribs with Genmix assessment
+    PFA_trib[1:NumberOfYears, i] <- TributarySpawn[1:NumberOfYears, i] / TributaryFemProp[1:NumberOfYears, i] +
       MainStemCatch[1:NumberOfYears, i]
     PFA_MS[1:NumberOfYears, i] <- PFA_trib[1:NumberOfYears, i]
   } else {
-    PFA_trib[1:NumberOfYears, i] <- TributarySpawn[1:NumberOfYears, i]/TributaryFemProp[1:NumberOfYears, i]
+    PFA_trib[1:NumberOfYears, i] <- TributarySpawn[1:NumberOfYears, i] / TributaryFemProp[1:NumberOfYears, i]
     PFA_MS[1:NumberOfYears, i] <- PFA_trib[1:NumberOfYears, i] + MainStemCatch[1:NumberOfYears, i]
   }
-  PFA_MS_15[1:NumberOfYears, i] <- PFA_MS[1:NumberOfYears, i] * (1-river_list[i, "PropSize_1.5"])
+  PFA_MS_15[1:NumberOfYears, i] <- PFA_MS[1:NumberOfYears, i] * (1 - river_list[i, "PropSize_1.5"])
 }
 
 PFA15_prop <- prop.table(PFA_MS_15, 1)
 j <- which(coastal_MS_catch$Year == StartingYear)
-TanaCoastalCatch <- rowSums(coastal_MS_catch[j:(j+NumberOfYears-1), 2:10]) # columns 2 to 10 equals coastal regions
+TanaCoastalCatch <- rowSums(coastal_MS_catch[j:(j + NumberOfYears - 1), 2:10]) # columns 2 to 10 equals coastal regions
 StockCoastalCatch <- TanaCoastalCatch * PFA15_prop
 PFA_coast <- PFA_MS + StockCoastalCatch
 PFA_coast_fem <- PFA_coast * TributaryFemProp
@@ -101,15 +101,15 @@ StockTotalCatch <- StockCoastalCatch + MainStemCatch + TributaryCatch
 
 for (i in 1:NumberOfRivers) {
   for (j in 1:NumberOfYears) {
-    if(TributarySpawn[j, i]>river_list[i, "GBM_med"]) {
+    if(TributarySpawn[j, i] > river_list[i, "GBM_med"]) {
       OverExploitation[j, i] <- 0
-    } else if(PFA_coast_fem[j, i]>river_list[i, "GBM_med"]) {
-      OverExploitation[j, i] <- (river_list[i, "GBM_med"]-TributarySpawn[j, i])/river_list[i, "GBM_med"]
+    } else if(PFA_coast_fem[j, i] > river_list[i, "GBM_med"]) {
+      OverExploitation[j, i] <- (river_list[i, "GBM_med"] - TributarySpawn[j, i]) / river_list[i, "GBM_med"]
     } else {
       OverExploitation[j, i] <- StockTotalCatch[j, i] * TributaryFemProp[j, i] / river_list[i, "GBM_med"]
     }
-    if(PFA_coast_fem[j, i]>river_list[i, "GBM_med"]) {
-      MaxTotalExploitation[j, i] <- (PFA_coast_fem[j, i]-river_list[i, "GBM_med"])/PFA_coast_fem[j, i]
+    if(PFA_coast_fem[j, i] > river_list[i, "GBM_med"]) {
+      MaxTotalExploitation[j, i] <- (PFA_coast_fem[j, i] - river_list[i, "GBM_med"]) / PFA_coast_fem[j, i]
     } else {
       MaxTotalExploitation[j, i] <- 0
     }
@@ -129,18 +129,19 @@ AvgMaxTotalExpl <- vector()
 #SumPFACoast <- vector()
 
 SumRiverList <- paste(river_list[1:NumberOfRivers, "RiverFullName"])
-SumTribCatch <- colSums(TributaryCatch)/NumberOfYears
-SumMSCatch <- colSums(MainStemCatch)/NumberOfYears
-SumCoastCatch <- colSums(StockCoastalCatch)/NumberOfYears
-SumSpawning <- colSums(TributarySpawn/TributaryFemProp)/NumberOfYears
-#SumPFATrib <- colSums(PFA_trib)/NumberOfYears
-#SumPFAMS <- colSums(PFA_MS)/NumberOfYears
-#SumPFACoast <- colSums(PFA_coast)/NumberOfYears
+SumTribCatch <- colSums(TributaryCatch) / NumberOfYears
+SumMSCatch <- colSums(MainStemCatch) / NumberOfYears
+SumCoastCatch <- colSums(StockCoastalCatch) / NumberOfYears
+SumSpawning <- colSums(TributarySpawn / TributaryFemProp) / NumberOfYears
+#SumPFATrib <- colSums(PFA_trib) / NumberOfYears
+#SumPFAMS <- colSums(PFA_MS) / NumberOfYears
+#SumPFACoast <- colSums(PFA_coast) / NumberOfYears
 
 AvgOverExpl <- apply(OverExploitation, 2, mean)
 AvgMaxTotalExpl <- apply(MaxTotalExploitation, 2, mean)
 
-if(NumberOfYears>3) {
+if(NumberOfYears > 3) {
+  
   OverExpl1 <- OverExploitation[1, ]
   OverExpl2 <- OverExploitation[2, ]
   OverExpl3 <- OverExploitation[3, ]
